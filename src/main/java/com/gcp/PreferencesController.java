890447b14027c1ed17e8915a7127504a9f97273f
rm185422 <rm185422@192.168.0.7>
2020-08-11 11:26:26 +0530

    ok

--- src/main/java/com/gcp/PreferencesController.java
+++ src/main/java/com/gcp/PreferencesController.java
@@ -1,14 +1,20 @@
 package com.gcp;
 
+import javax.websocket.server.PathParam;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PreferencesController {
+	private static final Logger LOGGER = LogManager.getLogger(PreferencesController.class);
 	
-	@GetMapping(value="/pref")
-	public String getPrefences() {
-	return "<html><h1>CAPS Depoyed on GCP </h1></html>";
+	@GetMapping(value="/pref/{userid}")
+	public String getPrefences(@PathParam("userid") String userid) {
+	LOGGER.info("<html><h1>CAPS Depoyed on GCP user:</html> "+userid+"</h1>");
+	return "<html><h1>CAPS Depoyed on GCP user:"+userid+"</h1></html>";
 	}
 
 }
