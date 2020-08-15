673cf24f286e9027b00df88394b044f2c807d65c
rm185422 <rm185422@192.168.0.7>
2020-08-15 17:05:30 +0530

    ok

--- src/main/java/com/gcp/dao/PreSpecRepository.java
+++ src/main/java/com/gcp/dao/PreSpecRepository.java
@@ -3,5 +3,7 @@ package com.gcp.dao;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PreSpecRepository extends CrudRepository<PreferenceSpec,Long > {
+	
+	public  PreferenceSpec findByPrefkey(String key);
 
 }
