0ba9def1bbcf3251fe1978bd4009a85f0ffd8cb8
rm185422 <mishratech81@gmail.com>
2020-08-21 09:14:08 +0530

    changes for pkg

--- src/main/java/com/gcp/CapsgcppocApplication.java
+++ src/main/java/com/gcp/CapsgcppocApplication.java
@@ -2,7 +2,8 @@ package com.gcp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan("com.gcp.*")
 @SpringBootApplication
 public class CapsgcppocApplication {
 
